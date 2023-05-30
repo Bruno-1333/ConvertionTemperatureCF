75d11cda5751f4daf2f81402d7461b8736aa0ad1
Bruno-1333 <2295132@cegepoutaouais.qc.ca>
2023-05-30 08:59:04 -0400

    version2

--- app/src/main/java/com/brunoleonardo/convertiontemperaturecf/MainActivity.kt
+++ app/src/main/java/com/brunoleonardo/convertiontemperaturecf/MainActivity.kt
@@ -2,10 +2,13 @@ package com.brunoleonardo.convertiontemperaturecf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.brunoleonardo.convertiontemperaturecf.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding // le lien entre le fichier xml et l'activité
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // le lien entre le fichier xml et l'activité
+        setContentView(binding.root) // le lien entre le fichier xml et l'activité
     }
 }
